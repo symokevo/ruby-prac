f7b70897e464357f3532e1b1d619b2de83660490
simon_r <symokevo@gmail.com>
2025-08-11 22:46:46 +0300

    End of number variables

--- variables.rb
+++ variables.rb
@@ -6,4 +6,13 @@ puts 8 / 2 # Division
 puts 3 ** 2 # Exponent
 puts 8 % 3 # Modulus (find the remainder of a division)
 
+puts 17 / 5 # Does not print the decimal
+puts 17 / 5.0 # prints the decimal value
 
+# Casting method for numbers
+# to_f, to_i
+
+# some useful number methods
+puts 3.even?
+puts 4.even?
+puts 7.odd?
