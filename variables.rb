a528f9dbf546fde6f793494b5e2f9140b1611d5b
simon_r <symokevo@gmail.com>
2025-08-11 22:35:05 +0300

    Initialize the repo

---
+++ variables.rb
@@ -0,0 +1,9 @@
+# Basic Data Types
+puts 1 + 1 # addition
+puts 2 - 1 # subtraction
+puts 4 * 4 # Multiplication
+puts 8 / 2 # Division
+puts 3 ** 2 # Exponent
+puts 8 % 3 # Modulus (find the remainder of a division)
+
+
