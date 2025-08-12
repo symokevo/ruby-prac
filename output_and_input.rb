be26b45f120e749b4f0d59efc06786a8f4b2f5fe
simon_r <symokevo@gmail.com>
2025-08-12 23:15:41 +0300

    create input outup file

---
+++ output_and_input.rb
@@ -0,0 +1,11 @@
+# Output command
+# print "Hello, World!" # This will print without a newline at the end
+# puts "Hello, World!"  # This will print with a newline at the end
+#
+
+# INPUT COMMANDS
+# gets.chomp # This will read input from the user and remove the newline character at the end
+# name = gets.chomp # This will read input from the user and assign it to the variable
+# puts "Hello, #{name}" # This will print the input with a greeting
+
+#new_string = gets.chomp # This will read input from the user and remove the newline character at the end
