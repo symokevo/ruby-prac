1bc225915f9093922e8a1e1f8bf594476c73fba0
simon_r <symokevo@gmail.com>
2025-08-12 22:52:06 +0300

    End of Varibles

---
+++ variables.rb
@@ -0,0 +1,35 @@
+# How to name variables in Ruby
+# The name should be clear and descriptive, using lowercase letters and underscores to separate words.
+# For example, use `user_name` instead of `username` or `UserName`. use snake_case for variable names.
+# Avoid using reserved words or special characters in variable names.
+# Variable names should not start with a number or contain spaces.
+# Use meaningful names that reflect the purpose of the variable, making your code more readable and maintainable.
+
+#==========================#
+#       VARIABLES
+#==========================#
+
+# Declaring a variable and assigning a value to it
+name = "Osmon"
+can_drive = true 
+
+
+age = 18
+puts age
+
+# Reassigning the variable to override the previous value
+age = 18 + 9
+puts age
+
+# using -=, *=, /=, and += operators
+age -= 2
+puts age
+
+age *= 2
+puts age
+
+age /= 2
+puts age
+
+age += 3
+puts age
